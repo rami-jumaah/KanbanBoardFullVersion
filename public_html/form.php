b94b3b5f092aaf17d8dd45b0229db85ffd521ecc
Rami Jumaah <rami.jumaah@liip.ch>
2018-10-24 10:08:37 +0200

    Gather email and password value and print them out

--- public_html/form.php
+++ public_html/form.php
@@ -1,8 +1,33 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: rami
- * Date: 12.09.18
- * Time: 10:29 AM
- */
+
+
+// define variables and set to empty values
+$email = $password = $repeatPassword = "";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (!empty($_POST["email"])) {
+        $email = $_POST['email'];
+    }
+
+    if (!empty($_POST["password"])) {
+        $password = $_POST['password'];
+    }
+    if (!empty($_POST['repeatPassword']) && ($_POST[$repeatPassword] == $_POST['password'])){
+        $repeatPassword = $password;
+    }else{
+        $passwordError = "Password Doesn't match";
+
+    }
+}
+
+echo "Hello $email your password is $password";
+
+//function test_input($data) {
+//    $data = trim($data);
+//    $data = stripslashes($data);
+//    $data = htmlspecialchars($data);
+//    return $data;
+//}
 
