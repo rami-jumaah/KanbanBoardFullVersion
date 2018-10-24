b9df5fdf5b113c1ddf73cf1086328ef5c45cc6de
fabian bienz <fabian-bienz@users.noreply.github.com>
2018-10-24 08:35:54 +0200

    rest api started

---
+++ src/rest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fabia_ovv7omp
+ * Date: 10/24/2018
+ * Time: 8:20 AM
+ */
+
+session_start();
+
+
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+
+
+
+
+switch ($method) {
+	case 'GET':
+		break;
+	case 'POST':
+		break;
+	case 'PUT':
+		break;
+	case 'DELETE':
+		break;
+}
+
+
